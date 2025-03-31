34dffb182dfd10e314e824c32e37a722c1ff854a
Jean-Paul Mestres <jp.mestres@gmail.com>
2025-03-30 17:19:29 -0700

    removing api from prefix

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -21,7 +21,7 @@ function InputForm({ onSubmit }) {
 
     setIsLoading(true);
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/fact-check`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/fact-check`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
